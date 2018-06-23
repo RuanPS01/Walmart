3bc854473cc2bba56add2a040065d38ad948c9f2
RuanPS01 <30608019+RuanPS01@users.noreply.github.com>
2018-06-23 17:11:15 -0300

    Reajuste PPL
    SQL com as devidas alterações por enquanto.

--- SchemaBDwalmart.sql
+++ SchemaBDwalmart.sql
@@ -74,18 +74,28 @@ DELIMITER $$
 CREATE FUNCTION insertDataExamples() returns VARCHAR(40)
 BEGIN
 	insert into Produto values('0', '2.00', 'Macarrão', 'Alimento não perecível.', 'Maglioni', '0001010100', '12/12/12','15');
-    insert into Produto values('0', '3.00', 'Leite Integral', 'Alimento perecível.', 'ClasseA', '0001010100', '12/12/12','15');
-    insert into Produto values('0', '4.00', 'Margarina', 'Alimento perecível.', 'Qualy', '0001010100', '12/12/12','15');
-    insert into Produto values('0', '5.00', 'Manteiga', 'Alimento perecível.', 'Qualy', '0001010100', '12/12/12','15');
-    insert into Produto values('0', '6.00', 'Nescal', 'Alimento não perecível.', 'Nestle', '0001010100', '12/12/12','15');
-    insert into Produto values('0', '7.00', 'Papel higiênico', 'Limpeza.', 'Mili', '0001010100', '12/12/12','15');
-    insert into Produto values('0', '8.00', 'Sabonete', 'Limpeza.', 'Dove', '0001010100', '12/12/12','15');
-    insert into Produto values('0', '10.00', 'Detergente', 'Limpeza.', 'Ype', '0001010100', '12/12/12','15');
-    insert into Cliente values('','','','','','','');
-    insert into Cliente values('','','','','','');
-    insert into funcionario values('1','1319', 'Pedrao Polez', 'pedro@internet.feliz', '9998887', 'Rua Margaridas 12', '20/12/95', 'polez', 'root');
-	insert into funcionario values('0', '10323', 'Ruan Patrick', 'ruan@internet.feliz', '9998887', 'Rua Margaridas 12', '24/01/97', 'ruan', 'root');
-    insert into funcionario values('0', '10323', 'Ensley Ribeiro', 'ensley@internet.feliz', '9998887', 'Rua Margaridas 12', '00/00/00', 'ensley', 'root');
+    insert into Produto values('1', '3.00', 'Leite Integral', 'Alimento perecível.', 'ClasseA', '0001010100', '12/12/12','15');
+    insert into Produto values('2', '4.00', 'Margarina', 'Alimento perecível.', 'Qualy', '0001010100', '12/12/12','15');
+    insert into Produto values('3', '5.00', 'Manteiga', 'Alimento perecível.', 'Qualy', '0001010100', '12/12/12','15');
+    insert into Produto values('4', '6.00', 'Nescal', 'Alimento não perecível.', 'Nestle', '0001010100', '12/12/12','15');
+    insert into Produto values('5', '7.00', 'Papel higiênico', 'Limpeza.', 'Mili', '0001010100', '12/12/12','15');
+    insert into Produto values('6', '8.00', 'Sabonete', 'Limpeza.', 'Dove', '0001010100', '12/12/12','15');
+    insert into Produto values('7', '10.00', 'Detergente', 'Limpeza.', 'Ype', '0001010100', '12/12/12','15');
+    insert into Cliente values('8','11111111111','João Henrique','joao@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+	insert into Cliente values('1','22222222222','Pedro João','joao@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+	insert into Cliente values('2','33333333333','Luana Lourenço','luana@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+	insert into Cliente values('3','44444444444','Jack Jhonsom','jack@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+	insert into Cliente values('4','55555555555','Jonas Petra','jonas@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+	insert into Cliente values('5','66666666666','Cleiton Peregrino','cleiton@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+	insert into Cliente values('6','77777777777','Antonella Souza','antonella@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+	insert into Cliente values('7','88888888888','Bruno Eleno','bruno@internet.feliz','(35)988888888','Rua Logo ali','00/00/00');
+    insert into Remessa values('0','12/12/12','12:00','Bauducco','ruan','Alimento');
+	insert into Remessa values('1','13/12/12','13:00','Nestle','ruan','Alimento');
+	insert into Remessa values('2','14/12/12','14:00','Unileve','ruan','Limpeza');
+	insert into Remessa values('3','15/12/12','15:00','Panco','ruan','Alimento');
+    insert into funcionario values('0','1319', 'Pedrao Polez', 'pedro@internet.feliz', '9998887', 'Rua Margaridas 12', '20/12/95', 'polez', 'root');
+	insert into funcionario values('1', '10323', 'Ruan Patrick', 'ruan@internet.feliz', '9998887', 'Rua Margaridas 12', '24/01/97', 'ruan', 'root');
+    insert into funcionario values('2', '10323', 'Ensley Ribeiro', 'ensley@internet.feliz', '9998887', 'Rua Margaridas 12', '00/00/00', 'ensley', 'root');
     return 'Dados de exemplo inseridos com sucesso';
 END $$
 DELIMITER ;
